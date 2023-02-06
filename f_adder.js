19465b8cc549bc2616e91ab2a1a53c004e1cdb0e
benji <benjamin@techstarter.de>
2023-02-06 13:02:07 +0100

    first commit

---
+++ f_adder.js
@@ -0,0 +1,13 @@
+//Function that adds it's parameters to calculate a sum:
+function adder(x,y,z) {
+  return x+y+z;
+}
+
+//Defining parameters:
+let a = "combine "
+let b = "a sentence "
+let c = "with the adder function!"
+
+//Print the result
+console.log(adder(a,b,c))
+console.log(adder(1,2,3))
